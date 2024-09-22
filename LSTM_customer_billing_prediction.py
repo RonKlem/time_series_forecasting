ed8985f18eae6211c687dcab2205b46268ced0c7
ron klem <rklem88@yahoo.com>
2024-09-22 15:27:11 -0500

    Updated LSTM model for customer billing prediction

--- LSTM_customer_billing_prediction.py
+++ LSTM_customer_billing_prediction.py
@@ -1,11 +1,12 @@
-import os
 import torch
 import torch.nn as nn
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
 from torch.autograd import Variable
-from ray import tune
-from ray.tune import CLIReporter
-from ray.tune.schedulers import ASHAScheduler
-from sklearn.metrics import mean_squared_error, mean_absolute_error
+import matplotlib.pyplot as plt
+from torch.autograd import Variable
 
 class LSTM(nn.Module):
     """
@@ -18,7 +19,7 @@ class LSTM(nn.Module):
         hidden_size (int): The number of features in the hidden state h.
         seq_length (int): The sequence length of the time series data.
     """
-    def __init__(self, num_classes, input_size, hidden_size, num_layers):
+    def __init__(self, num_classes, input_size, hidden_size, num_layers, seq_length):
         """
         Initialize the LSTM model.
 
@@ -27,7 +28,9 @@ class LSTM(nn.Module):
             num_layers (int): The number of recurrent layers.
             input_size (int): The number of expected features in the input x.
             hidden_size (int): The number of features in the hidden state h.
+            seq_length (int): The sequence length of the time series data.
         """
+
         super(LSTM, self).__init__()
         self.num_classes = num_classes
         self.num_layers = num_layers
